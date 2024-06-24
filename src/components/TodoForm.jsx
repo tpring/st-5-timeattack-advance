de1431634211fb0df5a7d0a3c94548136b26e4bf
tpring <pororo1@naver.com>
2024-06-24 15:54:13 +0900

    1_to_tanstack 완료

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,45 +1,58 @@
-import { useState } from "react";
-import { todoApi } from "../api/todos";
+import { QueryClient, useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+import { useState } from 'react';
+// import { todoApi } from "../api/todos";
 
 export default function TodoForm({ fetchData }) {
-  const [title, setTitle] = useState("");
-  const [contents, setContents] = useState("");
+    const [title, setTitle] = useState('');
+    const [contents, setContents] = useState('');
+    const queryclient = new QueryClient();
+    // TODO: useMutation 으로 리팩터링 하세요.
 
-  // TODO: useMutation 으로 리팩터링 하세요.
-  const handleAddTodo = async (e) => {
-    e.preventDefault();
-    setTitle("");
-    setContents("");
-    await todoApi.post("/todos", {
-      id: Date.now().toString(),
-      title,
-      contents,
-      isCompleted: false,
-      createdAt: Date.now(),
+    const addTodo = async () => {
+        await axios.post('http://localhost:4000/todos', {
+            id: Date.now().toString(),
+            title,
+            contents,
+            isCompleted: false,
+            createdAt: Date.now(),
+        });
+    };
+
+    const mutation = useMutation(addTodo, {
+        onSuccess: () => {
+            queryclient.invalidateQueries(['todos']);
+            setTitle('');
+            setContents('');
+        },
     });
-    await fetchData();
-  };
 
-  return (
-    <form onSubmit={handleAddTodo}>
-      <label htmlFor="title">제목:</label>
-      <input
-        type="text"
-        id="title"
-        name="title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        required
-      />
-      <label htmlFor="contents">내용:</label>
-      <input
-        id="contents"
-        name="contents"
-        value={contents}
-        onChange={(e) => setContents(e.target.value)}
-        required
-      />
-      <button type="submit">추가하기</button>
-    </form>
-  );
+    const handleAddTodo = async (e) => {
+        e.preventDefault();
+        mutation.mutate();
+        // await fetchData();
+    };
+
+    return (
+        <form onSubmit={handleAddTodo}>
+            <label htmlFor="title">제목:</label>
+            <input
+                type="text"
+                id="title"
+                name="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                required
+            />
+            <label htmlFor="contents">내용:</label>
+            <input
+                id="contents"
+                name="contents"
+                value={contents}
+                onChange={(e) => setContents(e.target.value)}
+                required
+            />
+            <button type="submit">추가하기</button>
+        </form>
+    );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,37 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { todoApi } from "../api/todos";
-import TodoForm from "../components/TodoForm";
-import TodoList from "../components/TodoList";
+import { useState, useEffect } from 'react';
+import TodoForm from '../components/TodoForm';
+import TodoList from '../components/TodoList';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
 
 export default function Home() {
-  // TODO: useQuery 로 리팩터링 하세요.
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [data, setData] = useState([]);
+    // TODO: useQuery 로 리팩터링 하세요.
 
-  const fetchData = async () => {
-    try {
-      const response = await todoApi.get("/todos");
-      setData(response.data);
-    } catch (err) {
-      setError(err);
-    } finally {
-      setIsLoading(false);
+    const fetchData = async () => {
+        const response = await axios.get('http://localhost:4000/todos');
+        return response.data;
+    };
+
+    const { data, isLoading, error } = useQuery({ queryKey: ['todos'], queryFn: fetchData });
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    if (isLoading) {
+        return <div style={{ fontSize: 36 }}>로딩중...</div>;
     }
-  };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+    if (error) {
+        console.error(error);
+        return <div style={{ fontSize: 24 }}>에러가 발생했습니다: {error.message}</div>;
+    }
 
-  if (isLoading) {
-    return <div style={{ fontSize: 36 }}>로딩중...</div>;
-  }
-
-  if (error) {
-    console.error(error);
     return (
-      <div style={{ fontSize: 24 }}>에러가 발생했습니다: {error.message}</div>
+        <>
+            <h2>서버통신 투두리스트 by useState</h2>
+            <TodoForm fetchData={fetchData} />
+            <TodoList todos={data} />
+        </>
     );
-  }
-
-  return (
-    <>
-      <h2>서버통신 투두리스트 by useState</h2>
-      <TodoForm fetchData={fetchData} />
-      <TodoList todos={data} />
-    </>
-  );
 }
